79d90e86c3fdc7189c183edf3a8accbe6c65ad7a
PiotrZdziarski <pzdziarski01@gmail.com>
2018-11-17 17:12:29 +0100

    rendering style buttons

--- src/app.js
+++ src/app.js
@@ -114,7 +114,7 @@ function createToolbar(nav) {
 
 
 function createToolbarButton(toolbar) {
-    let button = createElem('div', ['fslightbox-toolbar-button']);
+    let button = createElem('div', ['fslightbox-toolbar-button', 'button-style']);
     button.appendChild(
         createSVGIcon('M 11.469 10 l 7.08 -7.08 c 0.406 -0.406 0.406 -1.064 0 -1.469 c -0.406 -0.406 -1.063 -0.406 -1.469 0 L 10 8.53 l -7.081 -7.08 c -0.406 -0.406 -1.064 -0.406 -1.469 0 c -0.406 0.406 -0.406 1.063 0 1.469 L 8.531 10 L 1.45 17.081 c -0.406 0.406 -0.406 1.064 0 1.469 c 0.203 0.203 0.469 0.304 0.735 0.304 c 0.266 0 0.531 -0.101 0.735 -0.304 L 10 11.469 l 7.08 7.081 c 0.203 0.203 0.469 0.304 0.735 0.304 c 0.267 0 0.532 -0.101 0.735 -0.304 c 0.406 -0.406 0.406 -1.064 0 -1.469 L 11.469 10 Z')
     );
@@ -129,18 +129,36 @@ function createDOM() {
     container.id = "fslightbox-container";
     document.body.appendChild(container);
 
+    renderSlideButtons(container);
     renderNav(container);
     //create media holder
     let media_holder = createElem('div', ['fslightbox-media-holder']);
-    media_holder.style.height = window.innerHeight - 106 + 'px';
+    media_holder.style.height = window.innerHeight + 'px';
+    window.onresize = function() {
+        media_holder.style.height = window.innerHeight + 'px';
+    };
     container.appendChild(media_holder);
 
     let image = createElem('img', ['fslightbox-single-source']);
-    image.src = 'images/1.jpeg';
+    image.src = 'images/4.jpeg';
     media_holder.appendChild(image);
+}
 
-    renderBottomNav(container);
-    console.log(1);
+
+function renderSlideButtons(container) {
+    let left_btn_container = createElem('div', ['fslightbox-slide-btn-container']);
+    let left_btn = createElem('div', ['fslightbox-slide-btn', 'button-style']);
+    let left_svg = createSVGIcon('M8.388,10.049l4.76-4.873c0.303-0.31,0.297-0.804-0.012-1.105c-0.309-0.304-0.803-0.293-1.105,0.012L6.726,9.516c-0.303,0.31-0.296,0.805,0.012,1.105l5.433,5.307c0.152,0.148,0.35,0.223,0.547,0.223c0.203,0,0.406-0.08,0.559-0.236c0.303-0.309,0.295-0.803-0.012-1.104L8.388,10.049z');
+    left_btn.appendChild(left_svg);
+    left_btn_container.appendChild(left_btn);
+    container.appendChild(left_btn_container);
+
+    let right_btn_container = createElem('div', ['fslightbox-slide-btn-container', 'fslightbox-slide-btn-right-container']);
+    let right_btn = createElem('div', ['fslightbox-slide-btn', 'button-style']);
+    let right_svg = createSVGIcon('M11.611,10.049l-4.76-4.873c-0.303-0.31-0.297-0.804,0.012-1.105c0.309-0.304,0.803-0.293,1.105,0.012l5.306,5.433c0.304,0.31,0.296,0.805-0.012,1.105L7.83,15.928c-0.152,0.148-0.35,0.223-0.547,0.223c-0.203,0-0.406-0.08-0.559-0.236c-0.303-0.309-0.295-0.803,0.012-1.104L11.611,10.049z');
+    right_btn.appendChild(right_svg);
+    right_btn_container.appendChild(right_btn);
+    container.appendChild(right_btn_container);
 }
 function renderSingleSource(src) {
     let singleSource = document.createElement('img');
