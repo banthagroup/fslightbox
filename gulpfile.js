23e6de1a9e4e29ad586b7d3b82ebf880100cc5b7
PiotrZdziarski <pzdziarski01@gmail.com>
2018-11-19 22:47:48 +0100

    dimensions for source

--- gulpfile.js
+++ gulpfile.js
@@ -23,13 +23,14 @@ gulp.task('sass', function () {
     return gulp.src('src/scss/root.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.reload({ stream: true}));
+        .pipe(browserSync.reload({stream: true}));
 });
 
+
 gulp.task('js', function () {
-     return gulp.src('src/js/**/*.js')
-         .pipe(concat('app.js'))
-         .pipe(gulp.dest('src'));
+    return gulp.src('src/js/**/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('src'));
 });
 
 gulp.task('default', ['serve']);
