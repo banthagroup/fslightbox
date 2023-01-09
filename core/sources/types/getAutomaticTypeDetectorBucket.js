85743edd6783162df6b40b24ba2be542ba969464
piotrzdziarski <piotr.m.zdziarski@protonmail.com>
2023-01-09 15:25:04 +0100

    The lightbox now detects the short form of a YouTube URL.
    The one with the "youtu.be" host name.

--- core/sources/types/getAutomaticTypeDetectorBucket.js
+++ core/sources/types/getAutomaticTypeDetectorBucket.js
@@ -1,13 +1,14 @@
 export function getAutomaticTypeDetectorBucket() {
     return {
         isUrlYoutubeOne: (url) => {
-            const parser = document.createElement('a');
+            const parser = document.createElement("a");
             parser.href = url;
-            return parser.hostname === 'www.youtube.com';
+            return parser.hostname === "www.youtube.com"
+		|| parser.hostname === "youtu.be";
         },
 
         getTypeFromResponseContentType: (responseContentType) => {
-            return responseContentType.slice(0, responseContentType.indexOf('/'));
+            return responseContentType.slice(0, responseContentType.indexOf("/"));
         }
     };
 }
