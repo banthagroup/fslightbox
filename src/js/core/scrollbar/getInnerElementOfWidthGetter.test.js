d231c983a26289e2207fa3bf6d9b4e706c6f0c6e
piotrzdziarski <piotr.m.zdziarski@protonmail.com>
2023-01-09 07:38:16 +0100

    Removed testing.
    The time spent to write tests and the complication which it adds to the project (lots of configurations, packages, less or more reliable) don't compensate the time to spent fixing bugs caused by not doing so.

--- src/js/core/scrollbar/getInnerElementOfWidthGetter.test.js
+++
@@ -1,7 +0,0 @@
-import { getInnerElementOfWidthGetter } from "./getInnerElementOfWidthGetter";
-
-test('returning div element with right stylesElement', () => {
-    const inner = getInnerElementOfWidthGetter();
-    expect(inner.style.width).toBe('100%');
-    expect(inner.tagName).toBe('DIV');
-});
