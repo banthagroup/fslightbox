94742aeb18699e3975c1e388e126bda1b7514e41
piotrzdziarski <piotr.m.zdziarski@protonmail.com>
2023-01-30 17:34:32 +0100

    Fixed stage indexes in the slide index changer and hid non-stage smw elements on their render.

--- c/slide/setUpSlideIndexChanger.js
+++ c/slide/setUpSlideIndexChanger.js
@@ -31,7 +31,6 @@ export function setUpSlideIndexChanger(
     self.jumpTo = (i) => {
         var opi=stageIndexes.previous,oi=stageIndexes.current,oni=stageIndexes.next,ipl=isl[oi],icl=isl[i];
         self.changeTo(i);
-	var pi=stageIndexes.previous,ni=stageIndexes.next;
 
 	for (var j = 0; j < smw.length; j++) {
 		smw[j].d();
@@ -42,13 +41,15 @@ export function setUpSlideIndexChanger(
 
 	requestAnimationFrame(function(){
 		requestAnimationFrame(function(){
+			var pi=stageIndexes.previous,ni=stageIndexes.next;
+
 			// The checking whether the source is loaded was done before reflow because if source would load after a slide change but before a reflow, inexpected animation may be added—in the case of a current source, a slide change animation would be added instead of initial animation after a load.
 			if (ipl) {
 				sourceAnimationWrappers[oi]
 					.classList.add(FADE_OUT_CLASS_NAME);
 			}
 			if (icl) {
-				sourceAnimationWrappers[i]
+				sourceAnimationWrappers[stageIndexes.current]
 					.classList.add(FADE_IN_CLASS_NAME);
 			}
 
@@ -57,7 +58,7 @@ export function setUpSlideIndexChanger(
 			if (pi !== undefined && pi !== oi) {
 				smw[pi].ne();
 			}
-			smw[i].n();
+			smw[stageIndexes.current].n();
 			if (ni !== undefined && ni !== oi) {
 				smw[ni].p();
 			}
@@ -76,9 +77,9 @@ export function setUpSlideIndexChanger(
 				if (!stageManager.i(oi)) {
 					smw[oi].h();
 					smw[oi].n();
-				} else if (oi === pi) {
+				} else if (oi === stageIndexes.previous) {
 					smw[oi].ne();
-				} else if (oi === ni) {
+				} else if (oi === stageIndexes.next) {
 					smw[oi].p();
 				}
 			}
