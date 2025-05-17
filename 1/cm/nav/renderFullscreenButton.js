57ec1361125ee33401348fc94e88730572473f28
piotrzdziarski <pzdziarski@protonmail.com>
2025-05-17 21:53:23 +0200

    The fullscreen button's dimensions are no longer set through HTML attributes.

--- 1/cm/nav/renderFullscreenButton.js
+++ 1/cm/nav/renderFullscreenButton.js
@@ -4,11 +4,9 @@ import{PREFIX}from"../../cn/classes-names.js"
 
 export function renderFullscreenButton(o,p) {
 if(o.hfs)return;
-    const enterSize = '20px';
     const enterViewBox = '0 0 18 18';
     const enterD = 'M4.5 11H3v4h4v-1.5H4.5V11zM3 7h1.5V4.5H7V3H3v4zm10.5 6.5H11V15h4v-4h-1.5v2.5zM11 3v1.5h2.5V7H15V3h-4z';
 
-    const exitSize = '24px';
     const exitViewBox = '0 0 950 1024';
     const exitD = 'M682 342h128v84h-212v-212h84v128zM598 810v-212h212v84h-128v128h-84zM342 342v-128h84v212h-212v-84h128zM214 682v-84h212v212h-84v-128h-128z';
 
